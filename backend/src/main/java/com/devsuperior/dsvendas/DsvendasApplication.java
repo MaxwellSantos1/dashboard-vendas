6ba655ff67013fdf12a79c20f947082fd9423e38
Maxwell <maxwell-9@hotmail.com>
2021-09-11 15:41:02 -0300

    Primeira requisicao

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -9,17 +9,4 @@ public class DsvendasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsvendasApplication.class, args);
 	}
-
 }
-
-
-postgres:
-     //
-	qtubwpbqmmiyfd
-	:
-	ae7664acbb141d87136a842fc255f9cc8a6c6babb12b3d1db462085bee6277a1
-	@
-	ec2-44-193-228-249.compute-1.amazonaws.com
-	:
-	5432
-	/daa4grqgdtq29n
