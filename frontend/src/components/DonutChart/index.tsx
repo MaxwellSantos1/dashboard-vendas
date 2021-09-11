d90abe116b7953c6c8a72bd4e9275f191e027d7f
Maxwell <maxwell-9@hotmail.com>
2021-09-11 16:34:00 -0300

    Integracao Barchart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,13 +25,6 @@ const DonutChart = () => {
             });
     }, [])
 
-
-
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    //labels: ['Marcos', 'Joao', 'Max', 'Tomas', 'Lucas']
-    //}
-
     const options = {
         legend: {
             show: true
